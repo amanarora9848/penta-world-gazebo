f20251e8ea9986e0ff8b039dac23d27bf7125c55
amanarora9848 <aman.arora9848@gmail.com>
2021-08-10 20:17:26 +0530

    Committed all necessary files

---
+++ script/hello.cpp
@@ -0,0 +1,17 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Welcome to Aman's World!\nPlease ignore the crazy sideways moving bot which seems to be defying physics ;)\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
